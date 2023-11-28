12a0d265ce281d778240a78f912714f5161e67e8
utsho <utshodebravestone@gmail.com>
2023-11-28 21:51:28 +0600

    added menus in primary-nav

--- src/components/PrimaryNav.vue
+++ src/components/PrimaryNav.vue
@@ -2,13 +2,32 @@
   <nav
     class="fixed top-0 left-0 w-full flex justify-between items-center px-6 py-3 border-b border-custom-gray-1"
   >
-    <a class="text-3xl">{{ brand }}</a>
+    <a class="text-4xl" data-test="brand">{{ brand }}</a>
+
+    <ul class="flex items-center gap-10">
+      <li v-for="menu in menus" :key="menu.name" data-test="menu">
+        <a
+          :href="menu.link"
+          class="text-xl hover:opacity-50 transition-opacity duration-300"
+          >{{ menu.name }}</a
+        >
+      </li>
+    </ul>
   </nav>
 </template>
 
 <script>
 export default {
   name: "PrimaryNav",
-  data: () => ({ brand: "Tech Jobs" }),
+  data: () => ({
+    brand: "Tech Jobs",
+    menus: [
+      { name: "Teams", link: "" },
+      { name: "Location", link: "" },
+      { name: "Career in Tech", link: "" },
+      { name: "Students", link: "" },
+      { name: "Jobs", link: "" },
+    ],
+  }),
 };
 </script>
--- tests/PrimaryNav.test.js
+++ tests/PrimaryNav.test.js
@@ -5,6 +5,19 @@ import PrimaryNav from "@/components/PrimaryNav.vue";
 describe("test primary nav", () => {
   it("display the correct brand name", () => {
     const wrapper = mount(PrimaryNav);
-    expect(wrapper.text()).toMatch("Tech Jobs");
+    const brand = wrapper.find("[data-test='brand']");
+    expect(brand.text()).toEqual("Tech Jobs");
+  });
+
+  it("display menu items", () => {
+    const wrapper = mount(PrimaryNav);
+    const menus = wrapper.findAll("[data-test='menu']").map((it) => it.text());
+    expect(menus).toEqual([
+      "Teams",
+      "Location",
+      "Career in Tech",
+      "Students",
+      "Jobs",
+    ]);
   });
 });
